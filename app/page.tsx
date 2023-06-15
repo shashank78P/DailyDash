3b0abd1d824cb271a8c945669f32ac9afd0c0c41
Shashank P <96535169+shashank78P@users.noreply.github.com>
2023-06-16 01:51:14 +0530

    push to main2

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main className="bg-black h-screen w-full">
       <h1 className='text-green-500'>Jai ganesha</h1>
-      <Logo />
     </main>
   )
 }
