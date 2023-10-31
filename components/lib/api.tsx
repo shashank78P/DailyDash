f19381703ca03e299af9ec1c7eb475185034d178
Shashank P <96535169+shashank78P@users.noreply.github.com>
2023-10-31 15:16:49 +0530

    Update api.tsx

--- components/lib/api.tsx
+++ components/lib/api.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 // import * from "dotenv/config"
+console.log(process.env.NEXT_PUBLIC_BACKEND_API_URL)
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
     // + '/api',
